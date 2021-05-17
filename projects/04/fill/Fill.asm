bb491833b9fdd01bbf6dc73b63cdcc60ea67863b
sapjax <sapjax@gmail.com>
2021-05-17 11:21:57 +0800

    complete project 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,85 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@color
+M=0
+
+@SCREEN
+D=A
+@addr
+M=D
+
+@KBD
+D=A
+@max
+M=D
+
+(LOOP)
+
+  // if addr = max, goto observer
+  @max
+  D=M
+  @addr
+  D=D-M
+  @OBSERVER
+  D;JEQ
+
+  // draw color
+  @color
+  D=M
+  @addr
+  A=M
+  M=D
+
+  // set addr = addr + 16
+  @addr
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+
+(SETBLANK)
+  // if blank, do nothing
+  @color
+  D=M+1
+  @OBSERVER
+  D;JEQ
+
+  // set color to blank, then draw
+  @color
+  M=-1
+  @LOOP
+  0;JMP
+
+(SETWHITE)
+  // if white, do nothing
+  @color
+  D=M
+  @OBSERVER
+  D;JEQ
+
+  // set color to white, then draw
+  @color
+  M=0
+  @LOOP
+  0;JMP
+
+(OBSERVER)
+  // reset addr
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+  @KBD
+  D=M
+  @SETWHITE
+  D;JEQ
+  @SETBLANK
+  D;JNE
+
+(END)
+  @END
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,44 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// index = 0
+@index
+M=0
+@product
+M=0
+
+@R1
+D=M
+@times
+M=D
+
+// loop
+(LOOP)
+  // if index > R1, stop to end
+  @times
+  D=M
+  @index
+  D=D-M
+  @STOP
+  D;JEQ
+
+  @R0
+  D=M
+  @product
+  M=M+D
+  @index
+  M=M+1
+  @LOOP
+  0;JMP
+
+(STOP)  
+  @product
+  D=M
+  @R2
+  M=D
+
+// end
+(END)
+  @END
+  0;JMP
